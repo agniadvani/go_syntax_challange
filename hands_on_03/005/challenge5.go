5287de7693fb8fecb04b158990166a8e594d8131
agni-advani <advani.agni.agni@gmail.com>
2020-05-08 21:21:15 +0530

    hands on 3

---
+++ hands_on_03/005/challenge5.go
@@ -0,0 +1,15 @@
+//Create a program that shows the “if-else statement” in action.
+
+package main
+
+import "fmt"
+
+func main() {
+	weather := "Rainy"
+
+	if weather == "Rainy" {
+		fmt.Println("Carry an umbrella")
+	} else {
+		fmt.Println("Don't carry an umbrella")
+	}
+}
