7d922d01ffa96f8375417adacf0324a1c1677fe4
agni-advani <advani.agni.agni@gmail.com>
2020-05-08 20:45:58 +0530

    Hands on 2

---
+++ hands_on_02/004/challenge4.go
@@ -0,0 +1,19 @@
+//Using iota, create 4 constants for the last 4 years. Print the constant values.
+
+package main
+
+import "fmt"
+
+const (
+	a = 2020 - iota
+	b = 2020 - iota
+	c = 2020 - iota
+	d = 2020 - iota
+)
+
+func main() {
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+	fmt.Println(d)
+}
