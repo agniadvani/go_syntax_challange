f020633dd8568257d2e4b638b6d61b930eccb897
agni-advani <advani.agni.agni@gmail.com>
2020-05-08 20:31:41 +0530

    hands on 2

---
+++ hands_on_02/002/002/challenge2.go
@@ -0,0 +1,13 @@
+//Create TYPED and UNTYPED constants. Print the values of the constants.
+package main
+
+import "fmt"
+
+const (
+	a int = 4
+	b     = 45
+)
+
+func main() {
+	fmt.Println(a, b)
+}
