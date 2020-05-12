bcf52fd22b95ba144e40a84b1389ca9aff5cfac3
agni-advani <advani.agni.agni@gmail.com>
2020-05-13 03:42:44 +0530

    hands on 8

--- hands_on_08/challenge1.go
+++ hands_on_08/challenge1.go
@@ -1,6 +1,6 @@
 //in addition to the main goroutine, launch two additional goroutines
 //each additional goroutine should print something out
-//use waitgroups to make sure each goroutine finishes before your program exists
+//use waitgroups to make sure each goroutine finishes before your program exists.
 
 package main
 
