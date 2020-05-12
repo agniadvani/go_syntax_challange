7f6b2d7a5cb28624a0f1ddb454226f068c418bff
agni-advani <advani.agni.agni@gmail.com>
2020-05-13 03:41:27 +0530

    hands on 7

---
+++ hands_on_08/challenge1.go
@@ -0,0 +1,35 @@
+//in addition to the main goroutine, launch two additional goroutines
+//each additional goroutine should print something out
+//use waitgroups to make sure each goroutine finishes before your program exists
+
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+)
+
+var wg sync.WaitGroup
+
+func main() {
+
+	fmt.Println("Before foo: ", runtime.NumGoroutine())
+	wg.Add(1)
+	go foo()
+	fmt.Println("After foo: ", runtime.NumGoroutine())
+	wg.Add(1)
+	go bar()
+	fmt.Println("After bar: ", runtime.NumGoroutine())
+	wg.Wait()
+}
+
+func foo() {
+	fmt.Println("This is foo.")
+	wg.Done()
+}
+
+func bar() {
+	fmt.Println("This is bar.")
+	wg.Done()
+}
